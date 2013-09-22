b3cddaa01f36ad592d03c80e065fde52cfa63abb
Son Tran-Nguyen <esente@gmail.com>
2013-09-21 23:21:04 -0500

    Support inline embed of files in the same gist
    In the main Markdown file, author can embed inline any file in the
    same gist by using tag `<%= files[filename] %>`
    
    It is possible by taking advantage of the eex template with the right
    bindings. In this case, we evaluate the HTML-converted Markdown
    using a `files` binding with list of embed HTML partial using the file
    name as key.
    
    The embed code using GitHub's Javascript, and fall back to a `pre`
    tag when `noscript`.

--- lib/gistsio/gist_handler.ex
+++ lib/gistsio/gist_handler.ex
@@ -18,7 +18,7 @@ defmodule GistsIO.GistHandler do
 		], req, state}
 	end
 
-	def resource_exists(req, state) do
+	def resource_exists(req, _state) do
 		# @TODO: Check if binding has username, and redirect if not the right one
 		case Req.binding :gist, req do
 			{:undefined, req} -> {:false, req, :index}
@@ -39,19 +39,36 @@ defmodule GistsIO.GistHandler do
 	def gist_html(req, gist) do
 		files = gist["files"]
 		{name, attrs} = Enum.filter(files, &Utils.is_markdown/1) |> Enum.at 0
-		content_html = attrs["content"] |> Kernel.bitstring_to_list 
+		# Acquire embed code for each file other than the main file
+		attachments = lc {n, _} inlist files, n !== name, do: {n, embed(gist, n)}
+		# Parse the Markdown into HTML, then evaluate any <%= files[filename] %> tag
+		# and replace with the corresponding embed code.
+		# This way the author can embed any file in his/her gist any where in the article.
+		content_html = attrs["content"] 
+						|> Kernel.bitstring_to_list 
 						|> :erlmarkdown.conv 
 						|> Kernel.list_to_bitstring
-		gist = gist |> Utils.prep_gist |> ListDict.put("html", content_html)
+						|> EEx.eval_string [files: attachments] # allow inline embed
 
+		# Then set up article's title using either the description or filename
+		gist = gist |> Utils.prep_gist 
+					|> ListDict.put("html", content_html)
+					|> ListDict.put("attachments", attachments)
+		# Render the gist's partial
 		gist_html = [:code.priv_dir(:gistsio), "templates", "gist.html.eex"]
 				|> Path.join
-				|> EEx.eval_file [entry: gist, main_file: name]
-
+				|> EEx.eval_file [entry: gist]
+		# Put it into the base layout
 		html = [:code.priv_dir(:gistsio), "templates", "base.html.eex"]
 				|> Path.join
 				|> EEx.eval_file [content: gist_html, title: gist["title"]]
 
 		{html, req, gist}
+	end
+
+	defp embed(gist, filename) do
+		[:code.priv_dir(:gistsio), "templates", "embed.html.eex"]
+		|> Path.join
+		|> EEx.eval_file [gist: gist, name: filename]
 	end
 end
