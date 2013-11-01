174f4aa914115fba82136aea5560c1b76e64106e
Son Tran-Nguyen <esente@gmail.com>
2013-11-01 08:34:46 -0500

    Make the teaser block always on top
    * The teaser block will be moved to the first position no matter where it
    was created.
    * Moving the teaser block using the Reorder will set it back to position 1
    * Moving other block using the Reorder to position 1 will just move it to
    position 2 instead
    * Because we have to tab, the markdown block's background can just
    be transparent to look like a content editable div like the rest.
    * Comment is now a Markdown block.
    
    @TODO: Maybe just move the other block to where it was?
    @TODO: Support dragging to change order too.

--- priv/static/js/teaser-block.js
+++ priv/static/js/teaser-block.js
@@ -12,15 +12,29 @@ SirTrevor.Blocks.Teaser = (function(){
             '<div class="tab-pane gio-preview"></div>',
         '</div>',
     ].join("\n"));
-
-
-
+    
     return SirTrevor.Blocks.Markdown.extend({
 
         type: "teaser",
 
         editorHTML: function() {
             return md_template(this);
+        },
+
+        onBlockRender: function() {
+            var editorID = this.instanceID, $el = this.$el,
+                editorInstance = _.findWhere(SirTrevor.instances, {"ID": editorID});
+            SirTrevor.EventBus.on(editorID + ":blocks:change_position", function($block, selectedPosition, beforeOrAfter) {
+                if ($block.is($el)) {
+                    editorInstance.changeBlockPosition($el, 1);
+                } else if (selectedPosition == "1") {
+                    editorInstance.changeBlockPosition($block, 2)
+                }
+            });
+
+            setTimeout(function() {
+                editorInstance.changeBlockPosition($el, 1);
+            }, 0);
         }
 
     })
